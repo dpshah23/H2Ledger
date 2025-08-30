54274b7c16bbef97dad71d98fc0d411f9a9002d9
Nakshi Shah <nakshishah3108@gmail.com>
2025-08-30 22:33:46 +0530

    urls

--- Backend/h2ledger/api/urls.py
+++ Backend/h2ledger/api/urls.py
@@ -3,4 +3,7 @@ from .views import *
 urlpatterns = [
     path("transfer/", transfer_credit, name="transfer_credit"),
     path("mint/", mint_credit, name="mint_credit"),
+    path("batch/", verify_batch, name="verify_batch"),
+    path("use/", use_credit, name="use_credit"),
+    path("dashboard/", dashboard_view, name="dashboard_view"),
 ]
